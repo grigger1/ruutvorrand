57a09e72acfdb4ed6e2da41bc2ce3a9cb909cb80
Karl Gregor Pihlak <kapihla@koolimaja.sise>
2018-10-12 16:10:10 +0300

    initial commit

---
+++ ruutvorrand.py
@@ -0,0 +1,8 @@
+# 1.Looge Githubis repro
+# 2.Kopeerige repro aadressi.
+# 3.Panete terminali ```git clone (aadress)```
+# 4.Teete kloonitud kaustas töö ära.
+# 5.Lähete terminalis sama kausta sisse.
+# 6.```git add .```
+# 7.```git commit -m {suvaline sõnum}```
+# 8.```git push```
